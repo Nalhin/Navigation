bdf029c6cf56e88ff4f02fe0d042c0bb65775268
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-03-13 23:03:09 +0100

    feat: file and fileBzip osm provider.

--- backend/parser/src/main/java/com/navigation/parser/provider/OSMProviderBzipFile.java
+++ backend/parser/src/main/java/com/navigation/parser/provider/OSMProviderBzipFile.java
@@ -8,16 +8,17 @@ import javax.xml.stream.XMLStreamReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class OSMProviderFile implements OSMProvider {
+public class OSMProviderBzipFile implements OSMProvider {
+
   private final String filePath;
 
-  public OSMProviderFile(String filePath) {
+  public OSMProviderBzipFile(String filePath) {
     this.filePath = filePath;
   }
 
   @Override
   public XMLStreamReader loadOSMXml() throws IOException, XMLStreamException {
     var xmlInputFactory = XMLInputFactory.newInstance();
-    return xmlInputFactory.createXMLStreamReader(new FileInputStream(filePath));
+    return xmlInputFactory.createXMLStreamReader(new BZip2CompressorInputStream(new FileInputStream(filePath)));
   }
 }
