11cdf52ab090230706d3c7c8d4c0a007f002d0f3
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-04-27 15:21:08 +0200

    feat: service discovery.

--- backend/services/osm-data-processor/src/main/java/com/navigation/osmdataprocessor/infrastructure/exporter/RootExporter.java
+++ backend/services/osm-data-processor/src/main/java/com/navigation/osmdataprocessor/infrastructure/exporter/RootExporter.java
@@ -8,25 +8,32 @@ import com.navigation.parser.provider.OSMProvider;
 import com.navigation.parser.provider.OSMProviderBzipFile;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Component
 public class RootExporter implements ApplicationListener<ApplicationReadyEvent> {
 
+  private final ApplicationContext context;
   private final OSMProvider provider;
   private final AddressExporter addressExporter;
   private final StreetExporter streetExporter;
   private final ExecutorService executorService = Executors.newFixedThreadPool(2);
 
   public RootExporter(
+      ApplicationContext context,
       @Value("${infrastructure.osm.filePath}") String filePath,
       AddressExporter addressExporter,
       StreetExporter streetExporter) {
+    this.context = context;
     provider = new OSMProviderBzipFile(filePath);
     this.addressExporter = addressExporter;
     this.streetExporter = streetExporter;
@@ -34,7 +41,20 @@ public class RootExporter implements ApplicationListener<ApplicationReadyEvent> 
 
   @Override
   public void onApplicationEvent(@NotNull ApplicationReadyEvent event) {
-    executorService.execute(() -> new AddressDataProcessor(provider, addressExporter).processAndExport());
-    executorService.execute(() -> new StreetDataProcessor(provider, streetExporter).processAndExport());
+    var address =
+        executorService.submit(
+            () -> new AddressDataProcessor(provider, addressExporter).processAndExport());
+    var street =
+        executorService.submit(
+            () -> new StreetDataProcessor(provider, streetExporter).processAndExport());
+
+    try {
+      address.get();
+      street.get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    } finally {
+      SpringApplication.exit(context, () -> 0);
+    }
   }
 }
