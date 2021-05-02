9becbb21464c66e6228c5d317ba6e8955e100379
Krzysztof Olipra <contact@krzysztofolipra.com>
2021-05-02 18:55:58 +0200

    feat: reverse geocoding swagger.

--- backend/services/reverse-geocoding-api/src/main/java/com/navigation/reversegeocodingapi/api/ReverseGeocodingController.java
+++ backend/services/reverse-geocoding-api/src/main/java/com/navigation/reversegeocodingapi/api/ReverseGeocodingController.java
@@ -3,8 +3,11 @@ package com.navigation.reversegeocodingapi.api;
 import com.navigation.reversegeocodingapi.api.response.AddressDto;
 import com.navigation.reversegeocodingapi.domain.Location;
 import com.navigation.reversegeocodingapi.domain.ReverseGeocodingService;
-import org.springframework.data.repository.query.Param;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 @Controller
+@Tag(name = "reverse-geocode")
 @RequestMapping("/api/v1/reverse-geocode")
 class ReverseGeocodingController {
 
@@ -24,9 +28,14 @@ class ReverseGeocodingController {
     this.mapper = mapper;
   }
 
-  @GetMapping
+  @Operation(
+      tags = "reverse-geocode",
+      description = "Find address closest to the given coordinates")
+  @ApiResponse(responseCode = "200", description = "Success")
+  @ApiResponse(responseCode = "404", description = "Address not found")
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<AddressDto> getClosestAddress(
-          @RequestParam("latitude") Double latitude, @RequestParam("longitude") Double longitude) {
+      @RequestParam("latitude") Double latitude, @RequestParam("longitude") Double longitude) {
 
     return service
         .findClosestAddress(new Location(latitude, longitude))
